25783c394024136638754f6c7d5691385ec9c6e5
kim-Joy <85498186+kim-Joy@users.noreply.github.com>
2023-04-19 11:13:47 +0900

    step option script modify
    확인시, 마이페이지 테마변경도 체크 부탁드립니다.

--- JS/common.js
+++ JS/common.js
@@ -21,9 +21,64 @@ $(function(){
   })
 
 
-  //옵션 & 테마 적용 
-  editodoWrap.classList.add('lightmode', 'style_simple', 'palette_simple'); // 디폴트 컬러 적용
-  editodoWrap.classList.add(mode, theme, color); // 선택된 옵션 적용
+  //모드 선택 
+  $(document).on('click', '.swiper-container .mode button', function(){
+    let optionli = $(this).parents('li');
+    const defaultMode = editodoWrap.classList[2];
+
+    optionli.addClass('on');
+    optionli.siblings().removeClass('on');
+
+    if($('.layout_option.mode > li').hasClass('on')){ 
+      let modebtn = this.classList[1];
+      editodoWrap.classList.replace(defaultMode, modebtn);
+      sessionStorage.setItem('mode' , modebtn);
+      if(modebtn == 'system') { //시스템 모드 따라가기
+        if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+          editodoWrap.classList.replace('system', 'darkmode')
+        } else {
+          editodoWrap.classList.replace('system', 'lightmode')
+        }
+      }
+    }
+  });
+
+  //테마 선택 
+  $(document).on('click', '.swiper-container .theme button', function(){
+    let optionli = $(this).parents('li');
+    const defaultTheme = editodoWrap.classList[3];
+
+    optionli.addClass('on');
+    optionli.siblings().removeClass('on');
+
+    if($('.layout_option.theme > li').hasClass('on')) { 
+      let themebtn = this.classList[1];
+      editodoWrap.classList.replace(defaultTheme, themebtn);
+      sessionStorage.setItem('theme' , themebtn);
+    }
+  });
+
+  //색상 선택 
+  $(document).on('click', '.swiper-container .color button', function(){
+    let optionli = $(this).parents('li');
+    const defaultColor = editodoWrap.classList[4];
+
+    optionli.addClass('on');
+    optionli.siblings().removeClass('on');
+
+    if($('.layout_option.color > li').hasClass('on')){ 
+      let colorbtn = this.classList[1];
+      editodoWrap.classList.replace(defaultColor, colorbtn);
+      sessionStorage.setItem('color' , colorbtn);
+    }
+  });
+
+
+
+
+  //옵션 & 테마 적용 디폴트 적용
+  editodoWrap.classList.add('lightmode', 'style_simple', 'palette_simple');
+  editodoWrap.classList.add(mode, theme, color);
 
   //날짜 출력
   const init = {
